3e1d57236d0ddd2cacdfb07fb36d4be94aa167a4
hojelse <k1@hojelse.dk>
2020-08-26 13:14:03 +0200

    Complete Exercise 5

--- Assignment00/unit-testing-using-dotnet-test/LeapYear.Tests/UnitTest1.cs
+++ Assignment00/unit-testing-using-dotnet-test/LeapYear.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using LeapYear;
+using System.IO;
 
 namespace LeapYear.Tests
 {
@@ -35,6 +36,46 @@ namespace LeapYear.Tests
         {
             bool actual = Program.IsLeapYear(year);
             bool expected = true;
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("1700")]
+        [InlineData("1800")]
+        [InlineData("1900")]
+        [InlineData("2100")]
+        public void Not_Leap_Year_Strings(string yearAsString)
+        {
+            string actual = Program.IsLeapYear_With_Strings(yearAsString);
+            string expected = "nay";
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("1600")]
+        [InlineData("2000")]
+        [InlineData("2104")]
+        public void Is_Leap_Year_Strings(string yearAsString)
+        {
+            string actual = Program.IsLeapYear_With_Strings(yearAsString);
+            string expected = "yay";
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("1")]
+        [InlineData("2")]
+        [InlineData("3")]
+        [InlineData("4")]
+        [InlineData("5")]
+        [InlineData("116")]
+        [InlineData("100")]
+        [InlineData("300")]
+        public void Is_Before_1582_Strings(string yearAsString)
+        {
+            string actual = Program.IsLeapYear_With_Strings(yearAsString);
+            string expected = "The leap year function only applies to years from 1582.";
             Assert.Equal(expected, actual);
         }
     }
--- Assignment00/unit-testing-using-dotnet-test/LeapYear/LeapYear.cs
+++ Assignment00/unit-testing-using-dotnet-test/LeapYear/LeapYear.cs
@@ -4,12 +4,24 @@ namespace LeapYear
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Check if a year is a leap year!");
+            Console.WriteLine("Enter a year:");
+
+            string yearAsString = Console.ReadLine();
+            Console.WriteLine(IsLeapYear_With_Strings(yearAsString));
         }
 
-        public static bool IsLeapYear(int year)
+    public static string IsLeapYear_With_Strings(string yearAsString)
+    {
+        bool isInt = int.TryParse(yearAsString, out int year);
+        if(!isInt) return "Not a Number";
+        if(year <= 1582) return "The leap year function only applies to years from 1582.";
+        return IsLeapYear(year) ? "yay" : "nay";
+    }
+
+    public static bool IsLeapYear(int year)
         {
             bool isLeapYear = false;
             if(year % 4 == 0) isLeapYear = true;
